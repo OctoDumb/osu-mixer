2e6c4ff42874bfff66a5fcb148a218c45598bcbd
OctopuSSX <vladik0701@gmail.com>
2020-02-24 18:03:35 +0300

    Separatable building

--- build.js
+++ build.js
@@ -1,4 +1,10 @@
 var Builder = require('electron-builder');
+var cla = require('command-line-args');
+const defs = [
+    { name: 'ia32', type: Boolean, defaultValue: false },
+    { name: 'x64', type: Boolean, defaultValue: false }
+];
+const opts = cla(defs);
 
 const config = {
     appId: 'org.octodumb.osumixer',
@@ -10,21 +16,25 @@ const config = {
 };
 
 (async function() {
-    console.log("Building x64")
-    var x64 = await Builder.build({
-        config: Object.assign(config, {
-            artifactName: "osu! mixer Setup ${version} win-x64.${ext}"
-        }),
-        x64: true
-    });
-    console.log("Finished building x64", x64);
+    if(opts.x64) {
+        console.log("Building x64")
+        var x64 = await Builder.build({
+            config: Object.assign(config, {
+                artifactName: "osu! mixer Setup ${version} win-x64.${ext}"
+            }),
+            x64: true
+        });
+        console.log("Finished building x64", x64);
+    }
 
-    console.log("Building ia32");
-    var ia32 = await Builder.build({
-        config: Object.assign(config, {
-            artifactName: "osu! mixer Setup ${version} win-ia32.${ext}"
-        }),
-        ia32: true
-    });
-    console.log("Finished building ia32", ia32);
+    if(opts.ia32) {
+        console.log("Building ia32");
+        var ia32 = await Builder.build({
+            config: Object.assign(config, {
+                artifactName: "osu! mixer Setup ${version} win-ia32.${ext}"
+            }),
+            ia32: true
+        });
+        console.log("Finished building ia32", ia32);
+    }
 })();
